c68a40893afeace2ad19807ccbda6917949b23a4
gowtham01raj <gowthamraj.ts@outlook.com>
2025-12-04 11:21:42 +0530

    add gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import NavBar from "./components/NavBar";
-
+import { NavBar, Welcome } from "#components";
 const App = () => {
   return (
     <main>
-      <NavBar />0
+      <NavBar />
+      <Welcome />
     </main>
   );
 };
